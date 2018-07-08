87917aa8b2780d41b6fcf3faa95039b690c5dbe9
Mike McKee <mikecm@gmail.com>
2018-07-08 13:28:19 -0600

    Adding ability to find words

--- lib/trie.rb
+++ lib/trie.rb
@@ -2,11 +2,11 @@
 
 require './lib/node'
 
-# defines the Trie method
+# defines the Trie class
 class Trie
   attr_accessor :root,
                 :count
-                
+
   def initialize
     @root = Node.new('*')
     @count = 0
@@ -44,5 +44,37 @@ class Trie
     @count += 1
   end
 
-  # ----------------- INFORMATION METHODS ---------------------------------
+  def populate(dictionary)
+    words = dictionary.split("\n")
+    words.each do |word|
+      add_word(word)
+    end
+  end
+
+  def find_character(letter, current_child)
+    current_child.find do |node_object|
+      letter == node_object.value
+    end
+  end
+
+  def include?(word)
+    find_word(word) do |found, current_child|
+      return found && current_child.is_word
+    end
+  end
+
+  def find_word(word)
+    letters = word.chars
+    current_child = @root
+
+    word_found = letters.all? do |letter|
+      current_child = find_character(letter, current_child.children)
+    end
+
+    if block_given?
+      yield(word_found, current_child)
+    end
+
+    base
+  end
 end
