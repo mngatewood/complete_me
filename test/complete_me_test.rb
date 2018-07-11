ab84c4e34dce9d54731be0b4ad71953f11870fae
Mike McKee <mikecm@gmail.com>
2018-07-11 12:53:59 -0600

    More updating tests

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -64,20 +64,10 @@ class CompleteMeTest < Minitest::Test
     assert_equal "doggerelist", cm.suggest("doggerel").first
   end
 
-  def test_it_deletes_words
+  def test_it_deletes_rwords
     cm.populate(large_word_list)
 
-    cm.delete_word('pizzeria')
-
-    expected = ["pize", "pizza", "pizzicato", "pizzle"]
-    actual = cm.suggest('piz')
-
-    assert_equal expected, actual
-  end
-
-  def test_it_deletes_words
-    cm.populate(large_word_list)
-
+    cm.select('piz', 'pizzeria')
     cm.delete_word('pizzeria')
 
     expected = ["pize", "pizza", "pizzicato", "pizzle"]
